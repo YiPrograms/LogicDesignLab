b8f269bf0fcb677dbec8b2d14f5dc90eb67e33d6
Yi Kuo <kuokuoyiyi@gmail.com>
2021-10-10 10:23:34 +0800

    feat: Finish lab3_2

--- lab3/lab3_2/lab3_2.srcs/sources_1/new/lab3_2.v
+++ lab3/lab3_2/lab3_2.srcs/sources_1/new/lab3_2.v
@@ -22,7 +22,7 @@ module lab3_2(
 );
 
     wire clk_d;
-    clock_divider #(2) cd(.clk(clk), .clk_div(clk_d));
+    clock_divider #(25) cd(.clk(clk), .clk_div(clk_d));
 
     parameter S_Flashing = 2'b00;
     parameter S_Shifting = 2'b01;
@@ -35,23 +35,21 @@ module lab3_2(
     reg [5:0] cnt;
     reg [5:0] next_cnt;
 
-    reg [5:0] scnt;
-    reg [5:0] next_scnt;
-
-    reg [5:0] ecnt;
-    reg [5:0] next_ecnt;
-
     reg [15:0] leds;
-    reg [15:0] next_leds;
+
+    reg [15:0] flashing_leds;
+    always @* begin : flashing_block
+        flashing_leds = (cnt & 1'b1)? {16 {1'b0}}: {16 {1'b1}};
+    end
 
     reg [15:0] shifting_leds;
     always @* begin : shifting_block
         integer i;
         for (i = 0; i < 16; i = i + 1) begin
-            if (i + 16 < scnt || i + 1 > scnt)
+            if (i + 16 < cnt || i + 1 > cnt)
                 shifting_leds[i] = 1'b0;
             else
-                shifting_leds[i] = (i + scnt) & 1'b1;
+                shifting_leds[i] = (i + cnt) & 1'b1;
         end
     end
 
@@ -61,62 +59,62 @@ module lab3_2(
 
         expanding_leds = {16 {1'b0}};
         for (i = 8; i < 16; i = i + 1)
-            if (i < 8 + ecnt)
+            if (i < 8 + cnt)
                 expanding_leds[i] = 1'b1;
         for (i = 7; i >= 0; i = i - 1)
-            if (i > 7 - ecnt)
+            if (i > 7 - cnt)
                 expanding_leds[i] = 1'b1;
     end
 
     always @* begin
         next_state = state;
-        next_leds = leds;
         next_cnt = cnt;
-        next_scnt = scnt;
-        next_ecnt = ecnt;
 
-        case (state)
-            S_Flashing: begin
-                if (cnt >= 11) begin
-                    next_state = S_Shifting;
-                    next_cnt = 0;
-                    next_leds = shifting_leds;
-                end else begin
-                    next_leds = ~leds;
-                    next_cnt = cnt + 1;
-                end
-            end
-
-            S_Shifting: begin
-                if (scnt == 0 || scnt == 32) begin
-                    next_state = S_Expanding;
-                    next_scnt = 15;
-                    next_leds = expanding_leds;
-                end else begin
-                    next_leds = shifting_leds;
-                    next_scnt = scnt + (dir == 1? 1: -1);
-                end
-            end
-
-            S_Expanding: begin
-                if (ecnt >= 8) begin
-                    next_state = S_Flashing;
-                    next_ecnt = 1 + (dir == 1? 1: -1);
-                    next_leds = {16 {1'b1}};
-                end else begin
-                    next_leds = expanding_leds;
-                    if (dir == 0)
-                        next_ecnt = ecnt + 1;
-                    else begin
-                        if (ecnt > 0)
-                            next_ecnt = ecnt - 1;
+        if (rst) begin
+            leds = {16 {1'b1}};
+        end else begin
+            leds = {16 {1'b0}};
+            case (state)
+                S_Flashing: begin
+                    leds = flashing_leds;
+                    if (cnt >= 11) begin
+                        next_state = S_Shifting;
+                        next_cnt = 16;
+                    end else begin
+                        next_cnt = cnt + 1;
                     end
                 end
-            end
 
-            S_Invalid:
-                next_state = S_Flashing;
-        endcase
+                S_Shifting: begin
+                    leds = shifting_leds;
+                    if (cnt == 0 || cnt == 31) begin
+                        next_state = S_Expanding;
+                        next_cnt = 1;
+                    end else begin
+                        next_cnt = cnt + (dir == 1? 1: -1);
+                    end
+                end
+
+                S_Expanding: begin
+                    leds = expanding_leds;
+                    if (cnt >= 7) begin
+                        next_state = S_Flashing;
+                        next_cnt = 0;
+                    end else begin
+                        if (dir == 0)
+                            next_cnt = cnt + 1;
+                        else begin
+                            if (cnt > 0)
+                                next_cnt = cnt - 1;
+                        end
+                    end
+                end
+
+                S_Invalid:
+                    next_state = S_Flashing;
+            endcase
+        end
+        
     end
 
     
@@ -125,15 +123,9 @@ module lab3_2(
         if (rst) begin
             state <= S_Flashing;
             cnt <= 0;
-            scnt <= 16;
-            ecnt <= 1;
-            leds <= {16 {1'b1}};
         end else if (en) begin
             state <= next_state;
             cnt <= next_cnt;
-            scnt <= next_scnt;
-            ecnt <= next_ecnt;
-            leds <= next_leds;
         end
     end
 
@@ -141,22 +133,22 @@ module lab3_2(
 
 endmodule
 
-module lab3_2_t;
-    reg clk;
+// module lab3_2_t;
+//     reg clk;
 
-    always #1 clk = ~clk;
+//     always #1 clk = ~clk;
 
-    reg rst;
+//     reg rst;
 
-    wire [15:0] led;
+//     wire [15:0] led;
 
-    lab3_2 l3(.clk(clk), .rst(rst), .en(1), .dir(0), .led(led));
+//     lab3_2 l3(.clk(clk), .rst(rst), .en(1), .dir(0), .led(led));
 
-    initial begin
-        clk = 0;
-        rst = 1;
-        #2
+//     initial begin
+//         clk = 0;
+//         rst = 1;
+//         #2
 
-        rst = 0;
-    end
-endmodule
+//         rst = 0;
+//     end
+// endmodule
