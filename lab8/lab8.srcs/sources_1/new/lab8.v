a4777fee186a10b630fd7e68c5e7e1aaf00bf12a
Yi Kuo <kuokuoyiyi@gmail.com>
2021-12-20 22:04:00 +0800

    feat: Add lab8

---
+++ lab8/lab8.srcs/sources_1/new/lab8.v
@@ -0,0 +1,518 @@
+`define c   32'd262   // C3
+`define g   32'd392   // G3
+`define b   32'd494   // B3
+`define hc  32'd524   // C4
+`define hd  32'd588   // D4
+`define he  32'd660   // E4
+`define hf  32'd698   // F4
+`define hg  32'd784   // G4
+
+`define c4   32'd262
+`define d4   32'd294
+`define e4   32'd330
+`define f4   32'd349
+`define g4   32'd392
+`define a4   32'd440
+`define b4   32'd494
+
+`define sil   32'd50000000 // slience
+
+module clock_divider(clk, clk_div);   
+    parameter n = 26;     
+    input clk;   
+    output clk_div;   
+    
+    reg [n-1:0] num;
+    wire [n-1:0] next_num;
+    
+    always@(posedge clk)begin
+    	num<=next_num;
+    end
+    
+    assign next_num = num +1;
+    assign clk_div = num[n-1];
+    
+endmodule
+
+
+module player_control (
+	input clk, 
+	input reset, 
+	input _play, 
+	input _slow, 
+	input _mode, 
+	output reg [11:0] ibeat
+);
+	parameter LEN = 4095;
+    reg [11:0] next_ibeat;
+
+	always @(posedge clk, posedge reset) begin
+		if (reset) begin
+			ibeat <= 0;
+		end else begin
+            ibeat <= next_ibeat;
+		end
+	end
+
+    always @* begin
+        next_ibeat = (ibeat + 1 < LEN) ? (ibeat + 1) : LEN-1;
+    end
+
+endmodule
+
+module note_gen(
+    clk, // clock from crystal
+    rst, // active high reset
+    volume, 
+    note_div_left, // div for note generation
+    note_div_right,
+    audio_left,
+    audio_right
+);
+
+    // I/O declaration
+    input clk; // clock from crystal
+    input rst; // active low reset
+    input [2:0] volume;
+    input [21:0] note_div_left, note_div_right; // div for note generation
+    output [15:0] audio_left, audio_right;
+
+    // Declare internal signals
+    reg [21:0] clk_cnt_next, clk_cnt;
+    reg [21:0] clk_cnt_next_2, clk_cnt_2;
+    reg b_clk, b_clk_next;
+    reg c_clk, c_clk_next;
+
+    // Note frequency generation
+    // clk_cnt, clk_cnt_2, b_clk, c_clk
+    always @(posedge clk or posedge rst)
+        if (rst == 1'b1)
+            begin
+                clk_cnt <= 22'd0;
+                clk_cnt_2 <= 22'd0;
+                b_clk <= 1'b0;
+                c_clk <= 1'b0;
+            end
+        else
+            begin
+                clk_cnt <= clk_cnt_next;
+                clk_cnt_2 <= clk_cnt_next_2;
+                b_clk <= b_clk_next;
+                c_clk <= c_clk_next;
+            end
+    
+    // clk_cnt_next, b_clk_next
+    always @*
+        if (clk_cnt == note_div_left)
+            begin
+                clk_cnt_next = 22'd0;
+                b_clk_next = ~b_clk;
+            end
+        else
+            begin
+                clk_cnt_next = clk_cnt + 1'b1;
+                b_clk_next = b_clk;
+            end
+
+    // clk_cnt_next_2, c_clk_next
+    always @*
+        if (clk_cnt_2 == note_div_right)
+            begin
+                clk_cnt_next_2 = 22'd0;
+                c_clk_next = ~c_clk;
+            end
+        else
+            begin
+                clk_cnt_next_2 = clk_cnt_2 + 1'b1;
+                c_clk_next = c_clk;
+            end
+
+    // Assign the amplitude of the note
+    // Volume is controlled here
+    assign audio_left = (note_div_left == 22'd1) ? 16'h0000 : 
+                                (b_clk == 1'b0) ? 16'hE000 : 16'h2000;
+    assign audio_right = (note_div_right == 22'd1) ? 16'h0000 : 
+                                (c_clk == 1'b0) ? 16'hE000 : 16'h2000;
+endmodule
+
+module keypress_controller (
+    input clk,
+    input rst,
+    inout PS2_DATA,
+    inout PS2_CLK,
+    output reg [6:0] key_pressed
+);
+
+	wire [511:0] key_down;
+	wire [8:0] last_change;
+	wire been_ready;
+
+    KeyboardDecoder key_de (
+		.key_down(key_down),
+		.last_change(last_change),
+		.key_valid(been_ready),
+		.PS2_DATA(PS2_DATA),
+		.PS2_CLK(PS2_CLK),
+		.rst(rst),
+		.clk(clk)
+	);
+
+	always @(posedge clk, posedge rst) begin
+		if (rst) begin
+            key_pressed = 0;
+		end else begin
+			if (been_ready) begin
+				case (last_change)
+                    {1'b0, 8'h1c}: // L Shift
+                        key_pressed[0] = key_down[last_change];
+                    {1'b0, 8'h1b}: // [
+                        key_pressed[1] = key_down[last_change];
+                    {1'b0, 8'h23}: // ]
+                        key_pressed[2] = key_down[last_change];
+                    {1'b0, 8'h2b}: // K
+                        key_pressed[3] = key_down[last_change];
+                    {1'b0, 8'h34}: // L
+                        key_pressed[4] = key_down[last_change];
+                    {1'b0, 8'h33}: // ;
+                        key_pressed[5] = key_down[last_change];
+                    {1'b0, 8'h3b}: // ;
+                        key_pressed[6] = key_down[last_change];
+                endcase
+			end
+		end
+	end
+
+endmodule
+
+
+module music_example (
+	input [11:0] ibeatNum,
+	input en,
+	output reg [31:0] toneL,
+    output reg [31:0] toneR
+);
+
+    always @* begin
+        if(en == 1) begin
+            case(ibeatNum)
+                // --- Measure 1 ---
+                12'd0: toneR = `hg;      12'd1: toneR = `hg; // HG (half-beat)
+                12'd2: toneR = `hg;      12'd3: toneR = `hg;
+                12'd4: toneR = `hg;      12'd5: toneR = `hg;
+                12'd6: toneR = `hg;      12'd7: toneR = `hg;
+                12'd8: toneR = `he;      12'd9: toneR = `he; // HE (half-beat)
+                12'd10: toneR = `he;     12'd11: toneR = `he;
+                12'd12: toneR = `he;     12'd13: toneR = `he;
+                12'd14: toneR = `he;     12'd15: toneR = `sil; // (Short break for repetitive notes: high E)
+
+                12'd16: toneR = `he;     12'd17: toneR = `he; // HE (one-beat)
+                12'd18: toneR = `he;     12'd19: toneR = `he;
+                12'd20: toneR = `he;     12'd21: toneR = `he;
+                12'd22: toneR = `he;     12'd23: toneR = `he;
+                12'd24: toneR = `he;     12'd25: toneR = `he;
+                12'd26: toneR = `he;     12'd27: toneR = `he;
+                12'd28: toneR = `he;     12'd29: toneR = `he;
+                12'd30: toneR = `he;     12'd31: toneR = `he;
+
+                12'd32: toneR = `hf;     12'd33: toneR = `hf; // HF (half-beat)
+                12'd34: toneR = `hf;     12'd35: toneR = `hf;
+                12'd36: toneR = `hf;     12'd37: toneR = `hf;
+                12'd38: toneR = `hf;     12'd39: toneR = `hf;
+                12'd40: toneR = `hd;     12'd41: toneR = `hd; // HD (half-beat)
+                12'd42: toneR = `hd;     12'd43: toneR = `hd;
+                12'd44: toneR = `hd;     12'd45: toneR = `hd;
+                12'd46: toneR = `hd;     12'd47: toneR = `sil; // (Short break for repetitive notes: high D)
+
+                12'd48: toneR = `hd;     12'd49: toneR = `hd; // HD (one-beat)
+                12'd50: toneR = `hd;     12'd51: toneR = `hd;
+                12'd52: toneR = `hd;     12'd53: toneR = `hd;
+                12'd54: toneR = `hd;     12'd55: toneR = `hd;
+                12'd56: toneR = `hd;     12'd57: toneR = `hd;
+                12'd58: toneR = `hd;     12'd59: toneR = `hd;
+                12'd60: toneR = `hd;     12'd61: toneR = `hd;
+                12'd62: toneR = `hd;     12'd63: toneR = `hd;
+
+                // --- Measure 2 ---
+                12'd64: toneR = `hc;     12'd65: toneR = `hc; // HC (half-beat)
+                12'd66: toneR = `hc;     12'd67: toneR = `hc;
+                12'd68: toneR = `hc;     12'd69: toneR = `hc;
+                12'd70: toneR = `hc;     12'd71: toneR = `hc;
+                12'd72: toneR = `hd;     12'd73: toneR = `hd; // HD (half-beat)
+                12'd74: toneR = `hd;     12'd75: toneR = `hd;
+                12'd76: toneR = `hd;     12'd77: toneR = `hd;
+                12'd78: toneR = `hd;     12'd79: toneR = `hd;
+
+                12'd80: toneR = `he;     12'd81: toneR = `he; // HE (half-beat)
+                12'd82: toneR = `he;     12'd83: toneR = `he;
+                12'd84: toneR = `he;     12'd85: toneR = `he;
+                12'd86: toneR = `he;     12'd87: toneR = `he;
+                12'd88: toneR = `hf;     12'd89: toneR = `hf; // HF (half-beat)
+                12'd90: toneR = `hf;     12'd91: toneR = `hf;
+                12'd92: toneR = `hf;     12'd93: toneR = `hf;
+                12'd94: toneR = `hf;     12'd95: toneR = `hf;
+
+                12'd96: toneR = `hg;     12'd97: toneR = `hg; // HG (half-beat)
+                12'd98: toneR = `hg;     12'd99: toneR = `hg;
+                12'd100: toneR = `hg;    12'd101: toneR = `hg;
+                12'd102: toneR = `hg;    12'd103: toneR = `sil; // (Short break for repetitive notes: high D)
+                12'd104: toneR = `hg;    12'd105: toneR = `hg; // HG (half-beat)
+                12'd106: toneR = `hg;    12'd107: toneR = `hg;
+                12'd108: toneR = `hg;    12'd109: toneR = `hg;
+                12'd110: toneR = `hg;    12'd111: toneR = `sil; // (Short break for repetitive notes: high D)
+
+                12'd112: toneR = `hg;    12'd113: toneR = `hg; // HG (one-beat)
+                12'd114: toneR = `hg;    12'd115: toneR = `hg;
+                12'd116: toneR = `hg;    12'd117: toneR = `hg;
+                12'd118: toneR = `hg;    12'd119: toneR = `hg;
+                12'd120: toneR = `hg;    12'd121: toneR = `hg;
+                12'd122: toneR = `hg;    12'd123: toneR = `hg;
+                12'd124: toneR = `hg;    12'd125: toneR = `hg;
+                12'd126: toneR = `hg;    12'd127: toneR = `hg;
+
+                default: toneR = `sil;
+            endcase
+        end else begin
+            toneR = `sil;
+        end
+    end
+
+    always @(*) begin
+        if(en == 1)begin
+            case(ibeatNum)
+                12'd0: toneL = `hc;  	12'd1: toneL = `hc; // HC (two-beat)
+                12'd2: toneL = `hc;  	12'd3: toneL = `hc;
+                12'd4: toneL = `hc;	    12'd5: toneL = `hc;
+                12'd6: toneL = `hc;  	12'd7: toneL = `hc;
+                12'd8: toneL = `hc;	    12'd9: toneL = `hc;
+                12'd10: toneL = `hc;	12'd11: toneL = `hc;
+                12'd12: toneL = `hc;	12'd13: toneL = `hc;
+                12'd14: toneL = `hc;	12'd15: toneL = `hc;
+
+                12'd16: toneL = `hc;	12'd17: toneL = `hc;
+                12'd18: toneL = `hc;	12'd19: toneL = `hc;
+                12'd20: toneL = `hc;	12'd21: toneL = `hc;
+                12'd22: toneL = `hc;	12'd23: toneL = `hc;
+                12'd24: toneL = `hc;	12'd25: toneL = `hc;
+                12'd26: toneL = `hc;	12'd27: toneL = `hc;
+                12'd28: toneL = `hc;	12'd29: toneL = `hc;
+                12'd30: toneL = `hc;	12'd31: toneL = `hc;
+
+                12'd32: toneL = `g;	    12'd33: toneL = `g; // G (one-beat)
+                12'd34: toneL = `g;	    12'd35: toneL = `g;
+                12'd36: toneL = `g;	    12'd37: toneL = `g;
+                12'd38: toneL = `g;	    12'd39: toneL = `g;
+                12'd40: toneL = `g;	    12'd41: toneL = `g;
+                12'd42: toneL = `g;	    12'd43: toneL = `g;
+                12'd44: toneL = `g;	    12'd45: toneL = `g;
+                12'd46: toneL = `g;	    12'd47: toneL = `g;
+
+                12'd48: toneL = `b;	    12'd49: toneL = `b; // B (one-beat)
+                12'd50: toneL = `b;	    12'd51: toneL = `b;
+                12'd52: toneL = `b;	    12'd53: toneL = `b;
+                12'd54: toneL = `b;	    12'd55: toneL = `b;
+                12'd56: toneL = `b;	    12'd57: toneL = `b;
+                12'd58: toneL = `b;	    12'd59: toneL = `b;
+                12'd60: toneL = `b;	    12'd61: toneL = `b;
+                12'd62: toneL = `b;	    12'd63: toneL = `b;
+
+                12'd64: toneL = `hc;	12'd65: toneL = `hc; // HC (two-beat)
+                12'd66: toneL = `hc;    12'd67: toneL = `hc;
+                12'd68: toneL = `hc;	12'd69: toneL = `hc;
+                12'd70: toneL = `hc;	12'd71: toneL = `hc;
+                12'd72: toneL = `hc;	12'd73: toneL = `hc;
+                12'd74: toneL = `hc;	12'd75: toneL = `hc;
+                12'd76: toneL = `hc;	12'd77: toneL = `hc;
+                12'd78: toneL = `hc;	12'd79: toneL = `hc;
+
+                12'd80: toneL = `hc;	12'd81: toneL = `hc;
+                12'd82: toneL = `hc;    12'd83: toneL = `hc;
+                12'd84: toneL = `hc;    12'd85: toneL = `hc;
+                12'd86: toneL = `hc;    12'd87: toneL = `hc;
+                12'd88: toneL = `hc;    12'd89: toneL = `hc;
+                12'd90: toneL = `hc;    12'd91: toneL = `hc;
+                12'd92: toneL = `hc;    12'd93: toneL = `hc;
+                12'd94: toneL = `hc;    12'd95: toneL = `hc;
+
+                12'd96: toneL = `g;	    12'd97: toneL = `g; // G (one-beat)
+                12'd98: toneL = `g; 	12'd99: toneL = `g;
+                12'd100: toneL = `g;	12'd101: toneL = `g;
+                12'd102: toneL = `g;	12'd103: toneL = `g;
+                12'd104: toneL = `g;	12'd105: toneL = `g;
+                12'd106: toneL = `g;	12'd107: toneL = `g;
+                12'd108: toneL = `g;	12'd109: toneL = `g;
+                12'd110: toneL = `g;	12'd111: toneL = `g;
+
+                12'd112: toneL = `b;	12'd113: toneL = `b; // B (one-beat)
+                12'd114: toneL = `b;	12'd115: toneL = `b;
+                12'd116: toneL = `b;	12'd117: toneL = `b;
+                12'd118: toneL = `b;	12'd119: toneL = `b;
+                12'd120: toneL = `b;	12'd121: toneL = `b;
+                12'd122: toneL = `b;	12'd123: toneL = `b;
+                12'd124: toneL = `b;	12'd125: toneL = `b;
+                12'd126: toneL = `b;	12'd127: toneL = `b;
+
+                default : toneL = `sil;
+            endcase
+        end
+        else begin
+            toneL = `sil;
+        end
+    end
+endmodule
+
+module play_mode_controller (
+    input [6:0] key_pressed,
+    output reg [31:0] play_tone
+);
+
+    always @* begin
+        if (key_pressed[0])
+            play_tone = `c4;
+        else if (key_pressed[1])
+            play_tone = `d4;
+        else if (key_pressed[2])
+            play_tone = `e4;
+        else if (key_pressed[3])
+            play_tone = `f4;
+        else if (key_pressed[4])
+            play_tone = `g4;
+        else if (key_pressed[5])
+            play_tone = `a4;
+        else if (key_pressed[6])
+            play_tone = `b4;
+        else
+            play_tone = `sil;
+    end
+
+endmodule
+
+module lab8(
+    clk,        // clock from crystal
+    rst,        // BTNC: active high reset
+    _play,      // SW0: Play/Pause
+    _mute,      // SW1: Mute
+    _slow,      // SW2: Slow
+    _music,     // SW3: Music
+    _mode,      // SW15: Mode
+    _volUP,     // BTNU: Vol up
+    _volDOWN,   // BTND: Vol down
+    _higherOCT, // BTNR: Oct higher
+    _lowerOCT,  // BTNL: Oct lower
+    PS2_DATA,   // Keyboard I/O
+    PS2_CLK,    // Keyboard I/O
+    _led,       // LED: [15:13] octave & [4:0] volume
+    audio_mclk, // master clock
+    audio_lrck, // left-right clock
+    audio_sck,  // serial clock
+    audio_sdin, // serial audio data input
+    DISPLAY,    // 7-seg
+    DIGIT       // 7-seg
+);
+
+    // I/O declaration
+    input clk; 
+    input rst; 
+    input _play, _mute, _slow, _music, _mode; 
+    input _volUP, _volDOWN, _higherOCT, _lowerOCT; 
+    inout PS2_DATA; 
+	inout PS2_CLK; 
+    output [15:0] _led; 
+    output audio_mclk; 
+    output audio_lrck; 
+    output audio_sck; 
+    output audio_sdin; 
+    output [6:0] DISPLAY; 
+    output [3:0] DIGIT; 
+    
+    // Modify these
+    assign _led = 16'b1110_0000_0001_1111;
+    assign DIGIT = 4'b0000;
+    assign DISPLAY = 7'b0111111;
+
+    // Internal Signal
+    wire [15:0] audio_in_left, audio_in_right;
+
+    wire [11:0] ibeatNum;               // Beat counter
+    wire [31:0] freqL, freqR;           // Raw frequency, produced by music module
+    wire [21:0] freq_outL, freq_outR;    // Processed frequency, adapted to the clock rate of Basys3
+
+    wire [6:0] key_pressed;
+    wire [31:0] play_tone;
+
+
+
+    // clkDiv22
+    wire clkDiv22;
+    clock_divider #(.n(22)) clock_22(.clk(clk), .clk_div(clkDiv22));    // for keyboard and audio
+
+    // Player Control
+    // [in]  reset, clock, _play, _slow, _music, and _mode
+    // [out] beat number
+    player_control #(.LEN(128)) playerCtrl_00 ( 
+        .clk(clkDiv22),
+        .reset(rst),
+        ._play(1'b1),
+        ._slow(1'b0), 
+        ._mode(1'b0),
+        .ibeat(ibeatNum)
+    );
+
+    // Music module
+    // [in]  beat number and en
+    // [out] left & right raw frequency
+    music_example music_00 (
+        .ibeatNum(ibeatNum),
+        .en(1'b1),
+        .toneL(freqL),
+        .toneR(freqR)
+    );
+
+
+    keypress_controller kp(
+        .clk(clk),
+        .rst(rst),
+        .PS2_DATA(PS2_DATA),
+        .PS2_CLK(PS2_CLK),
+        .key_pressed(key_pressed)
+    );
+
+    play_mode_controller pmc(
+        .key_pressed(key_pressed),
+        .play_tone(play_tone)
+    );
+
+
+
+    // freq_outL, freq_outR
+    // Note gen makes no sound, if freq_out = 50000000 / `silence = 1
+    // assign freq_outL = 50000000 / freqL;
+    // assign freq_outR = 50000000 / freqR;
+    assign freq_outL = 50000000 / play_tone;
+    assign freq_outR = 50000000 / play_tone;
+
+    // Note generation
+    // [in]  processed frequency
+    // [out] audio wave signal (using square wave here)
+    note_gen noteGen_00(
+        .clk(clk), 
+        .rst(rst), 
+        .volume(3'b000),
+        .note_div_left(freq_outL), 
+        .note_div_right(freq_outR), 
+        .audio_left(audio_in_left),     // left sound audio
+        .audio_right(audio_in_right)    // right sound audio
+    );
+
+    // Speaker controller
+    speaker_control sc(
+        .clk(clk), 
+        .rst(rst), 
+        .audio_in_left(audio_in_left),      // left channel audio data input
+        .audio_in_right(audio_in_right),    // right channel audio data input
+        .audio_mclk(audio_mclk),            // master clock
+        .audio_lrck(audio_lrck),            // left-right clock
+        .audio_sck(audio_sck),              // serial clock
+        .audio_sdin(audio_sdin)             // serial audio data input
+    );
+
+endmodule
